8b54eab660c3d01173d3f24579eba5f815be0ef9
shbunch <shawnbunch@gmail.com>
2023-08-01 23:09:27 -0500

    Finished building routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,14 +6,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  })
+    .then(data => res.json(data))
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  })
+    .then(data => res.json(data))
 });
+// be sure to include its associated Category and Tag data
 
 // create new product
 router.post('/', (req, res) => {
@@ -94,6 +108,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => res.json(data))
 });
 
 module.exports = router;
